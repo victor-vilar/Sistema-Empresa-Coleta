d2a3982279df2e2d2e48df9cd306cf9a91403d19
Victor Vilar <victor.svilar@gmail.com>
2023-12-03 15:36:08 -0300

    fix: adicionado comentarios e feito pequenos ajustes.

--- frontend/src/app/shared/services/crudbase.service.ts
+++ frontend/src/app/shared/services/crudbase.service.ts
@@ -10,48 +10,84 @@ export abstract class CrudBaseService<T>{
 
 
   private refreshRequired = new Subject<any>();
-  static BASE_URL:string = environment.LOCAL_API_URL;
+  private readonly BASE_URL:string = environment.LOCAL_API_URL;
   list:T[] = [];
   rota:string;
   protected http:HttpClient = inject(HttpClient);
   constructor(){ }
 
-
+  /**
+ * return this subject as observable
+ * The components can subscribe to this observable to
+ * get the updates comming from the send method
+ * @returns this subject
+ */
   refreshAllData(){
     return this.refreshRequired.asObservable();
   }
 
+  /**
+   * method to make this subject emmit an value;
+   *
+   * @param object the object that the components wants to receive
+   */
   send(object:any){
       this.refreshRequired.next(object);
   }
 
+  /**
+   * I dont remeber why a made this method and i dont know if a component is using it anymore :D
+   */
   sendNull(){
     this.refreshRequired.next(this.list);
   }
 
+  /**
+   * save T in api
+   * @param type T object
+   * @returns post response as an observable
+   */
   save(type:T):Observable<T>{
-    return  this.http.post<T>(CrudBaseService.BASE_URL + this.rota,type,{withCredentials:true});
+    return  this.http.post<T>(this.BASE_URL + this.rota,type,{withCredentials:true});
   }
 
+  /**
+   * get all T from api, updated this list and send the object to all subscribed objects
+   */
   getAll(){
-    this.http.get<T[]>(CrudBaseService.BASE_URL + this.rota,{withCredentials:true})
+    this.http.get<T[]>(this.BASE_URL + this.rota,{withCredentials:true})
     .subscribe(value => {
       this.list = value;
       this.send(this.list)
     });
   };
 
-
+  /**
+   * get a T from api with the passed id
+   * @param id of the T
+   * @returns T
+   */
   getById(id:number | string):Observable<T>{
-    return this.http.get<T>(CrudBaseService.BASE_URL + this.rota + '/' + id,{withCredentials:true});
+    return this.http.get<T>(this.BASE_URL + this.rota + '/' + id,{withCredentials:true});
   };
 
+  /**
+   *  update a T object
+   * @param type of the T
+   * @returns updated T
+   */
   update(type:T):Observable<T>{
-    return this.http.put<T>(CrudBaseService.BASE_URL + this.rota, type,{withCredentials:true});
+    return this.http.put<T>(this.BASE_URL + this.rota, type,{withCredentials:true});
   };
 
+
+  /**
+   *  delete T object from api
+   * @param id of the T
+   * @returns Observable T
+   */
   delete(id:number | string):Observable<T>{
-    return this.http.delete<T>(CrudBaseService.BASE_URL + this.rota + '/' + id,{withCredentials:true});
+    return this.http.delete<T>(this.BASE_URL + this.rota + '/' + id,{withCredentials:true});
   };
 
 
