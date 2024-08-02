220c3eb965420ab20414013aa2408c5f02cfa392
Victor Vilar <victor.svilar@gmail.com>
2024-08-01 21:07:02 -0300

    fix: removido comentarios.

--- backend/src/main/java/com/victorvilar/projetoempresa/services/EquipmentService.java
+++ backend/src/main/java/com/victorvilar/projetoempresa/services/EquipmentService.java
@@ -27,19 +27,9 @@ public class EquipmentService {
         this.equipmentMapper = equipmentMapper;
     }
 
-    /**
-     * get all equipments
-     * @return a list of equipments
-     */
     public List<EquipmentResponseDto> getAll(){
         return this.equipmentMapper.toEquipmentResponseDtoList(this.repository.findAll());
     }
-
-    /**
-     * get equipment by id
-     * @param id id of a equipment
-     * @return EquipmentResponseDto
-     */
 
     public EquipmentResponseDto getById(Long id){
         return this.equipmentMapper.toEquipmentResponseDto(
@@ -48,53 +38,31 @@ public class EquipmentService {
         );
     }
 
-    /**
-     * get equipment by id
-     * @param id id of a equipment
-     * @return equipment
-     */
     public Equipment findEquipmentById(Long id){
                 return this.repository.findById(id)
                 .orElseThrow(() -> new EquipmentNotFoundException("Equipment not found !"));
     }
 
-    /**
-     * save a new equipment
-     * @param equipmentCreateDto equipment to save
-     */
     @Transactional
     public EquipmentResponseDto save(EquipmentCreateDto equipmentCreateDto){
         Equipment equipment = this.equipmentMapper.toEquipament(equipmentCreateDto);
         return this.equipmentMapper.toEquipmentResponseDto(this.repository.save(equipment));
     }
 
-    /**
-     * update an equipment
-     * @param equipmentUpdateDto equipment instance to get the new data
-     * @return equipment updated
-     */
+    @Transactional
     public EquipmentResponseDto update(EquipmentUpdateDto equipmentUpdateDto){
         Equipment equipment = this.findEquipmentById(equipmentUpdateDto.getId());
         equipment.setEquipmentName(equipmentUpdateDto.getEquipmentName());
         equipment.setSizeInMeterCubic(equipmentUpdateDto.getSizeInMeterCubic());
-        this.repository.save(equipment);
-        return this.equipmentMapper.toEquipmentResponseDto(equipment);
+        return this.equipmentMapper.toEquipmentResponseDto(this.repository.save(equipment));
 
     }
 
-    /**
-     * delete a equipment
-     * @param id id to update
-     */
     @Transactional
     public void delete(Long id){
         this.repository.delete(this.findEquipmentById(id));
     }
 
-    /**
-     * get the total of entitys persisted
-     * @return integer of the count
-     */
     public Integer getEntityCount(){
         return this.repository.getEntityCount();
     }
