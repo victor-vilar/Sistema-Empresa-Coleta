441dca13b3328b572d8176ded8a46e744b7583a4
Victor Vilar <victor.svilar@gmail.com>
2024-08-19 15:20:40 -0300

    fix: atualizado a classe para implementar metodos da interface ServiceRequestDto e organizado o codigo para uma melhor visualização.

--- backend/src/main/java/com/victorvilar/projetoempresa/dto/serviceorder/ServiceOrderCreateDto.java
+++ backend/src/main/java/com/victorvilar/projetoempresa/dto/serviceorder/ServiceOrderCreateDto.java
@@ -2,59 +2,138 @@ package com.victorvilar.projetoempresa.dto.serviceorder;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.victorvilar.projetoempresa.dto.serviceorder.interfaces.ServiceOrderDto;
+import com.victorvilar.projetoempresa.dto.serviceorder.interfaces.ServiceOrderRequestDto;
+import com.victorvilar.projetoempresa.enums.ServiceOrderStatus;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
-public class ServiceOrderCreateDto implements ServiceOrderDto {
+public class ServiceOrderCreateDto implements ServiceOrderRequestDto {
 
 
     @JsonDeserialize(using = LocalDateDeserializer.class)
     @NotNull(message="the order must have a expected service day")
     private LocalDate serviceExpectedDate;
+
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    private LocalDate serviceExecutedDate;
+
+    private Long vehicle;
+
     @NotNull(message="The order must have an item from one contract")
     private Long itemContract;
+
+    private String ineaManifest;
+    private LocalTime serviceTime;
+    private String observation;
+    private String osFileUrl;
+
     @NotNull(message="The order must have an address")
     private Long address;
 
-    private String observation;
-    private Long vehicle;
+    private Long amountCollected;
 
-    public LocalDate getServiceExpectedDate() {
-        return serviceExpectedDate;
-    }
 
-    public void setServiceExpectedDate(LocalDate serviceExpectedDate) {
-        this.serviceExpectedDate = serviceExpectedDate;
-    }
 
+    @Override
     public Long getItemContract() {
-        return itemContract;
+        return this.itemContract;
     }
 
-    public void setItemContract(Long itemContract) {
-        this.itemContract = itemContract;
-    }
-
+    @Override
     public Long getAddress() {
-        return address;
+        return this.address;
+    }
+
+    @Override
+    public Long getVehicle() {
+        return this.vehicle;
+    }
+
+    @Override
+    public ServiceOrderStatus getStatus() {
+        if(this.serviceExecutedDate != null && this.amountCollected != null){
+            return ServiceOrderStatus.DONE;
+        }
+        return ServiceOrderStatus.UNDONE;
+    }
+
+    @Override
+    public LocalDate getEmissionDate() {
+        return null;
+    }
+
+    @Override
+    public LocalDate getServiceExpectedDate() {
+        return this.serviceExpectedDate;
+    }
+
+    @Override
+    public LocalDate serviceExecutedDate() {
+        return this.serviceExecutedDate;
+    }
+
+    @Override
+    public String getObservation() {
+        return this.observation;
+    }
+
+    @Override
+    public LocalTime getServiceTime() {
+        return this.serviceTime;
+    }
+
+    @Override
+    public Long getAmountCollected() {
+        return this.amountCollected;
+    }
+
+    @Override
+    public String getIneaManifest() {
+        return this.ineaManifest;
+    }
+
+    @Override
+    public String getOsFileUrl() {
+        return this.osFileUrl;
     }
 
     public void setAddress(Long address) {
         this.address = address;
     }
 
-    public String getObservation() {
-        return observation;
+    public void setAmountCollected(Long amountCollected) {
+        this.amountCollected = amountCollected;
+    }
+
+    public void setIneaManifest(String ineaManifest) {
+        this.ineaManifest = ineaManifest;
+    }
+
+    public void setItemContract(Long itemContract) {
+        this.itemContract = itemContract;
     }
 
     public void setObservation(String observation) {
         this.observation = observation;
     }
 
-    @Override
-    public Long getVehicle() {
-        return this.vehicle;
+    public void setOsFileUrl(String osFileUrl) {
+        this.osFileUrl = osFileUrl;
+    }
+
+    public void setServiceExecutedDate(LocalDate serviceExecutedDate) {
+        this.serviceExecutedDate = serviceExecutedDate;
+    }
+
+    public void setServiceExpectedDate(LocalDate serviceExpectedDate) {
+        this.serviceExpectedDate = serviceExpectedDate;
+    }
+
+    public void setServiceTime(LocalTime serviceTime) {
+        this.serviceTime = serviceTime;
     }
 
     public void setVehicle(Long vehicle) {
