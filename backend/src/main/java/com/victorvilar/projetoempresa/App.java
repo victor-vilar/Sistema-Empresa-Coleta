5c8a0d806c8b82989618ce06be14bfd5e8d3d687
Victor Vilar <victor.svilar@gmail.com>
2024-08-26 14:42:03 -0300

    feat: adicionando anotação para habilitação do caching na aplicação.

--- backend/src/main/java/com/victorvilar/projetoempresa/App.java
+++ backend/src/main/java/com/victorvilar/projetoempresa/App.java
@@ -3,8 +3,10 @@ package com.victorvilar.projetoempresa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication(exclude= SecurityAutoConfiguration.class)
+@EnableCaching
 public class App {
 
 	public static void main(String[] args) {
