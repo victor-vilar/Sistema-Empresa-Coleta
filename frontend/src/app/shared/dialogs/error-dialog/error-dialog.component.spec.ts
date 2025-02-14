330f60d8788e669b8734570e3b252122418c02c0
Victor Vilar <victor.svilar@gmail.com>
2025-02-14 15:29:25 -0300

    fix/text: criado testes dos metodos do ErrorDialogComponent e do SuccessDialogComponent, criado tambem umas propriedades na view para ser mais facil de encontrar as tags que estão sendo testadas.

--- frontend/src/app/shared/dialogs/error-dialog/error-dialog.component.spec.ts
+++ frontend/src/app/shared/dialogs/error-dialog/error-dialog.component.spec.ts
@@ -1,14 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ErrorDialogComponent } from './error-dialog.component';
+import { SharedModule } from '../../shared.module';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { By } from '@angular/platform-browser';
 
 describe('ErrorDialogComponent', () => {
   let component: ErrorDialogComponent;
   let fixture: ComponentFixture<ErrorDialogComponent>;
+  let data = {error:'this is a error test message'};
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ErrorDialogComponent ]
+      declarations: [ ErrorDialogComponent ],
+      imports:[SharedModule],
+      providers:[
+        {provide: MatDialogRef,useValue: {}},
+        {provide: MAT_DIALOG_DATA,useValue: data}
+      ]
     })
     .compileComponents();
 
@@ -20,4 +29,12 @@ describe('ErrorDialogComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render error message in the view on init' ,() => {
+    let div = fixture.debugElement.query(By.css('[data-test-div="error-message-div"]')).nativeElement;
+    component.ngOnInit();
+    fixture.detectChanges();
+    expect(component.errorMessage).toBe(data.error);
+    expect(div.innerText).toBe(component.errorMessage);
+  })
 });
--- frontend/src/app/shared/dialogs/success-dialog/success-dialog.component.spec.ts
+++ frontend/src/app/shared/dialogs/success-dialog/success-dialog.component.spec.ts
@@ -1,14 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SuccessDialogComponent } from './success-dialog.component';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { SharedModule } from '../../shared.module';
+import { By } from '@angular/platform-browser';
 
-describe('SuccessDialogComponent', () => {
+fdescribe('SuccessDialogComponent', () => {
   let component: SuccessDialogComponent;
   let fixture: ComponentFixture<SuccessDialogComponent>;
+  let data = {message:'this is a successfull message'}
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SuccessDialogComponent ]
+      declarations: [ SuccessDialogComponent ],
+      imports:[SharedModule],
+      providers:[
+        {provide: MatDialogRef,useValue: {}},
+        {provide: MAT_DIALOG_DATA,useValue: data}
+      ]
     })
     .compileComponents();
 
@@ -20,4 +29,12 @@ describe('SuccessDialogComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+    it('should render the message in the view on init' ,() => {
+      let div = fixture.debugElement.query(By.css('[data-test-div="error-message-div"]')).nativeElement;
+      component.ngOnInit();
+      fixture.detectChanges();
+      expect(component.message).toBe(data.message);
+      expect(div.innerText).toBe(component.message);
+    })
 });
