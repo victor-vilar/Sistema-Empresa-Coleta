62554ded6516d8d8bc84492b590d4b75d41471af
Victor Vilar <victor.svilar@gmail.com>
2025-02-22 19:02:23 -0300

    test:Criado teste para o método de login de LoginService.

--- frontend/src/app/login/services/login.service.spec.ts
+++ frontend/src/app/login/services/login.service.spec.ts
@@ -2,41 +2,73 @@ import { DialogServiceService } from "src/app/shared/services/dialog-service.ser
 import { LoginService } from "./login.service";
 import { TestBed } from "@angular/core/testing";
 import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpResponse } from "@angular/common/http";
 import { Router } from "@angular/router";
+import { environment } from "src/environments/environment";
+import { ApplicationUser } from "src/app/shared/entities/ApplicationUser";
+import { of } from "rxjs";
 
-describe('LoginService',() => {
+fdescribe('LoginService',() => {
 
 
     let loginService:LoginService;
-    let httpClient:HttpClient;
+    let http:HttpClient;
     let httpTestingController:HttpTestingController;
     let router:jasmine.SpyObj<Router>;
     let dialogService:jasmine.SpyObj<DialogServiceService>
-    let appUser:any;
+    let notLoggedUser:any;
+    let loggedUser:any;
+    let createHeadersSpy:any;
+    let mockHttpResponse:any;
+    let mockHttpHeader:any;
 
     beforeEach(async() =>{
 
         await TestBed.configureTestingModule({
             providers:[
                 LoginService,
-                HttpClientTestingModule,
                 {provide:Router,useValue:jasmine.createSpyObj('Router',['navigate'])},
-                {provide:DialogServiceService,useValue:jasmine.createSpyObj('DialogService',['openProgressDialog'])}
-            ]
+                {provide:DialogServiceService,useValue:jasmine.createSpyObj('DialogService',['openProgressDialog', 'closeProgressSpinnerDialog'])}
+            ],
+            imports:[HttpClientTestingModule]
         })
 
         loginService = TestBed.inject(LoginService);
-        httpClient = TestBed.inject(HttpClient);
+        http = TestBed.inject(HttpClient);
         router = TestBed.inject(Router) as jasmine.SpyObj<Router>
         dialogService = TestBed.inject(DialogServiceService) as jasmine.SpyObj<DialogServiceService>
         httpTestingController = TestBed.inject(HttpTestingController);
-        appUser = {username:'Madruguinha',password:'123456',roles:['Gerente'], profilePhotoUrl:'/m.jpg'};
-        window.sessionStorage.setItem('loggedUser',JSON.stringify(appUser))
+
+        loggedUser = {username:'Madruguinha',roles:['Gerente'], profilePhotoUrl:'/m.jpg'};
+        notLoggedUser = {username:'Madruguinha', password:'123456'};
+        window.sessionStorage.setItem('loggedUser',JSON.stringify(loggedUser))
+        createHeadersSpy = spyOn<any>(loginService,'createHeaders');
+
+
+        mockHttpHeader = {
+            Authorization:'this is a mock token',
+            get: function(att:string){
+                return this['att'];
+            }
+        }
+
+        mockHttpResponse = {
+            headers: mockHttpHeader,
+            body:loggedUser
+        }
     })
 
 
     it('Should test the login method successfully',() =>{
+
+        let url = environment.API_URL + environment.API_VERSION + 'login';
+        let getSpy = spyOn(http,'get').and.returnValue(of(mockHttpResponse));
+        
+        loginService.login(notLoggedUser);
+
+        expect(getSpy).toHaveBeenCalled();
+        expect(dialogService.openProgressDialog).toHaveBeenCalled();
+        expect(createHeadersSpy).toHaveBeenCalled();
 
 
 
