d698f0e0bb16f69c4bc4cb8e64a9bef589ccd054
Victor Vilar <victor.svilar@gmail.com>
2025-02-25 09:35:05 -0300

    style:Configurado arquivo de testes do LoginComponent.

--- frontend/src/app/login/components/main/login-main.component.spec.ts
+++ frontend/src/app/login/components/main/login-main.component.spec.ts
@@ -1,20 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginMainComponent } from './login-main.component';
+import { Router } from '@angular/router';
+import { DialogServiceService } from 'src/app/shared/services/dialog-service.service';
 
 describe('LoginMainComponent', () => {
   let component: LoginMainComponent;
   let fixture: ComponentFixture<LoginMainComponent>;
+  let router:jasmine.SpyObj<Router>;
+  let dialogService:jasmine.SpyObj<DialogServiceService>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginMainComponent ]
+      declarations: [ LoginMainComponent ],
+      providers:[
+        {provide:Router,useValue:jasmine.createSpyObj('Router',['navigate'])},
+        {provide:DialogServiceService,useValue:jasmine.createSpyObj('DialogService',['openProgressDialog', 'closeProgressSpinnerDialog'])}
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(LoginMainComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
+    dialogService = TestBed.inject(DialogServiceService) as jasmine.SpyObj<DialogServiceService>;
+
   });
 
   it('should create', () => {
