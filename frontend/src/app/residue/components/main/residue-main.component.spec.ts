55272ca42a4d608e9e344d2cc8fcd1d035646908
Victor Vilar <victor.svilar@gmail.com>
2025-02-28 13:54:56 -0300

    test/fix:Criado metodo de teste para checar se estava se increvendo no obserable de queryParams do ActivatedRoute.

--- frontend/src/app/residue/components/main/residue-main.component.spec.ts
+++ frontend/src/app/residue/components/main/residue-main.component.spec.ts
@@ -3,7 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ResidueMainComponent } from './residue-main.component';
 import { ResiduesService } from '../../services/residues.service';
 import { DialogServiceService } from 'src/app/shared/services/dialog-service.service';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ActivatedRoute, convertToParamMap, Params, Router } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -15,17 +15,18 @@ fdescribe('ResidueMainComponent', () => {
   let fixture: ComponentFixture<ResidueMainComponent>;
   let residueService:jasmine.SpyObj<ResiduesService>;
   let dialogService:jasmine.SpyObj<DialogServiceService>;
-  let activatedRoute:ActivatedRoute
+  let activatedRoute:ActivatedRouteMock;
   
   beforeEach(async () => {
     
-    let mock = new ActivatedRouteMock();
+    activatedRoute = new ActivatedRouteMock();
+
     await TestBed.configureTestingModule({
       declarations: [ ResidueMainComponent ],
       providers:[
-        {provide:ActivatedRoute,useValue:mock},
+        {provide:ActivatedRoute,useValue:activatedRoute},//{queryParams:of(convertToParamMap({'dialog':true}))}},
         {provide:ResiduesService,userValue:jasmine.createSpyObj('ResiduesService',['getAll'])},
-        {provide:DialogServiceService,useValue:jasmine.createSpyObj('DialogService',['openProgressDialog', 'openErrorDialog'])},
+        {provide:DialogServiceService,useValue:jasmine.createSpyObj('DialogService',['openDialog', 'openErrorDialog'])},
         
       ],
       imports:[FormsModule,SharedModule,HttpClientModule]
@@ -36,17 +37,21 @@ fdescribe('ResidueMainComponent', () => {
     fixture = TestBed.createComponent(ResidueMainComponent);
     residueService = TestBed.inject(ResiduesService) as jasmine.SpyObj<ResiduesService>;
     dialogService = TestBed.inject(DialogServiceService) as jasmine.SpyObj<DialogServiceService>;
-    activatedRoute = TestBed.inject(ActivatedRoute);
     component = fixture.componentInstance;
     fixture.detectChanges();
 
-
-    //setando retorno dos metodos
-    
   });
 
   it('should create', () => {  
     expect(component).toBeTruthy();
+  });
+
+  it('should call the super.onInit and subscribe to queryParams',() => {
+    
+    spyOn(component,'openDialog');
+    component.ngOnInit();
+    activatedRoute.testParams = {dialog:true}
+    expect(component.openDialog).toHaveBeenCalled();
   });
 
 
