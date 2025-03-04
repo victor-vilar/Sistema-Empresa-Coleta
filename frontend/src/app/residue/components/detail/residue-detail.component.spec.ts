c231cf47a97d609eaf02f18ae7960abe3e444afe
Victor Vilar <victor.svilar@gmail.com>
2025-03-04 11:08:07 -0300

    test:Configurada dependências do arquivo de testes do ResidueDetailComponent.

--- frontend/src/app/residue/components/detail/residue-detail.component.spec.ts
+++ frontend/src/app/residue/components/detail/residue-detail.component.spec.ts
@@ -1,21 +1,57 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ResidueDetailComponent } from './residue-detail.component';
 import { Residue } from 'src/app/shared/entities/Residue';
 import { HttpClientModule } from '@angular/common/http';
 import { ResidueModule } from '../../modules/residue.module';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { DialogServiceService } from 'src/app/shared/services/dialog-service.service';
+import { ActivatedRouteMock } from 'src/app/tests/activated-route-mock';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ResiduesService } from '../../services/residues.service';
+import { ErrorsHelperService } from 'src/app/shared/services/erros-helper.service';
+import { ErrorStateMatcher } from '@angular/material/core';
 
-describe('ResidueDetailComponent', () => {
+fdescribe('ResidueDetailComponent', () => {
     let component: ResidueDetailComponent;
+    let fixture: ComponentFixture<ResidueDetailComponent>;
+    
+    //dependencies
+    let activatedRoute:ActivatedRouteMock;
+    let router:jasmine.SpyObj<Router>;
+    let dialogService:jasmine.SpyObj<DialogServiceService>;
+    let residueService:jasmine.SpyObj<ResiduesService>;
+    let errorHelper:jasmine.SpyObj<ErrorsHelperService>;
   
     beforeEach(() => {
+      
+      activatedRoute = new ActivatedRouteMock();
+
       TestBed.configureTestingModule(
         {
-            imports:[HttpClientModule,ResidueModule]
+            imports:[ResidueModule],
+            declarations:[ResidueDetailComponent],
+            providers:[
+              {provide:ActivatedRoute,useValue:activatedRoute},       
+              {provide:Router,useValue:jasmine.createSpyObj('Router',[''])},
+              {provide:DialogServiceService,useValue:jasmine.createSpyObj('DialogService',['openDialog', 'openErrorDialog'])},
+              {provide:ResiduesService,userValue:jasmine.createSpyObj('ResiduesService',['getAll'])},
+              {provide:ErrorsHelperService,useValue:jasmine.createSpyObj('ErrosHelperService',[''])},
+              {provide: MatDialogRef,useValue: jasmine.createSpy('close')},
+              {provide: MAT_DIALOG_DATA,useValue: {}}
+            ]
         }
         );
-      component = TestBed.inject(ResidueDetailComponent);
+
+    //injetando dependencias
+    fixture = TestBed.createComponent(ResidueDetailComponent);
+    router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
+    dialogService = TestBed.inject(DialogServiceService) as jasmine.SpyObj<DialogServiceService>;
+    residueService = TestBed.inject(ResiduesService) as jasmine.SpyObj<ResiduesService>;
+    errorHelper = TestBed.inject(ErrorsHelperService) as jasmine.SpyObj<ErrorsHelperService>;
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
       
     });
   
